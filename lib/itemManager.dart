db448df862a171da8012b09cfcc5b4cf4cd5a4b8
Kanval Kalra <kanval87@gmail.com>
2019-11-02 11:10:46 +0530

    Refactored more - code is seprated now

---
+++ lib/itemManager.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import './item.dart';
+
+class ItemManager extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ItemManagerState();
+  }
+}
+
+class _ItemManagerState extends State<ItemManager> {
+  List<String> _items = ['Item 1'];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    String inputText = '';
+    TextFormField textFormField = TextFormField(
+      decoration: const InputDecoration(
+          icon: Icon(Icons.person),
+          hintText: 'Enter Text',
+          labelText: 'Name *'),
+      controller: TextEditingController(
+        text: '',
+      ),
+      onChanged: (value) {
+        inputText = value;
+      },
+    );
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: AlignmentDirectional.centerStart,
+          child: textFormField,
+        ),
+        Container(
+          alignment: AlignmentDirectional.centerEnd,
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                _items.add(inputText);
+                print('TextFormField value -> : ' + inputText);
+                textFormField.controller.clear();
+              });
+            },
+            child: Text('Press Me'),
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        Container(
+          alignment: AlignmentDirectional.centerEnd,
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                _items.clear();
+                textFormField.controller.clear();
+              });
+            },
+            child: Text('Clear list'),
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        Column(
+          children: _items.map((element) => Item(itemName: element)).toList(),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './item.dart';
+import './itemManager.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,74 +22,6 @@ class MyApp extends StatelessWidget {
             alignment: AlignmentDirectional.topStart,
             child: ItemManager(),
           )),
-    );
-  }
-}
-
-class ItemManager extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _ItemManagerState();
-  }
-}
-
-class _ItemManagerState extends State<ItemManager> {
-  List<String> _items = ['Item 1'];
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    String inputText = '';
-    TextFormField textFormField = TextFormField(
-      decoration: const InputDecoration(
-          icon: Icon(Icons.person),
-          hintText: 'Enter Text',
-          labelText: 'Name *'),
-      controller: TextEditingController(
-        text: '',
-      ),
-      onChanged: (value) {
-        inputText = value;
-      },
-    );
-    return Column(
-      children: <Widget>[
-        Container(
-          alignment: AlignmentDirectional.centerStart,
-          child: textFormField,
-        ),
-        Container(
-          alignment: AlignmentDirectional.centerEnd,
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                _items.add(inputText);
-                print('TextFormField value -> : ' + inputText);
-                textFormField.controller.clear();
-              });
-            },
-            child: Text('Press Me'),
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-        Container(
-          alignment: AlignmentDirectional.centerEnd,
-          child: RaisedButton(
-            onPressed: () {
-              setState(() {
-                _items.clear();
-                textFormField.controller.clear();
-              });
-            },
-            child: Text('Clear list'),
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-        Column(
-          children: _items.map((element) => Item(itemName: element)).toList(),
-        )
-      ],
     );
   }
 }
